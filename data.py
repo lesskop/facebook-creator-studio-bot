ffb11ca3336ab2ecf91975198c611fac1f64562d
lesskop <lesskop@mail.ru>
2021-03-12 15:47:30 +0300

    Part 2

--- data.py
+++ data.py
@@ -2,3 +2,43 @@ username = ''
 password = ''
 
 link = 'https://business.facebook.com/creatorstudio/home'
+
+num_of_photo = 0
+num_of_video = 0
+
+# write path to your content
+cats_photo = [rf'\photo ({i}).jpg' for i in range(1, num_of_photo + 1)]
+cats_video = [rf'\video ({i}).mp4' for i in range(1, num_of_video + 1)]
+cats_content = cats_video + cats_photo
+
+tags_cats = ['cats', 'catsofinstagram', 'cat', 'of', 'catstagram', 'instagram', 'catlovers', 'catlover', 'catlife',
+             'instacat', 'meow', 'pets', 'kittens', 'kitten', 'kitty', 'catoftheday', 'love', 'cute', 'pet', 'animals',
+             'dogs', 'gatos', 'world', 'gato', 'petsofinstagram', 'adoptdontshop', 'kittensofinstagram', 'cutecats',
+             'lovecats', 'bhfyp', 'cutecat', 'catsofig', 'catlove', 'instacats', 'petstagram', 'animal', 'dog',
+             'kittycat', 'ilovemycat', 'catslover', 'catsofinsta', 'catloversclub', 'catscatscats', 'instagood', 'day',
+             'dogsofinstagram', 'blackcat', 'katze', 'photooftheday', 'ilovecats', 'catsofworld', 'catsoftheworld',
+             'catworld', 'katzen', 'caturday', 'chat', 'neko', 'nature', 'gatosdeinstagram', 'photography']
+
+tags_follow = ['follow', 'like', 'love', 'instagood', 'instagram', 'photooftheday', 'followme', 'photography',
+               'likeforlikes', 'picoftheday', 'fashion', 'beautiful', 'me', 'instadaily', 'likes', 'smile',
+               'followforfollowback', 'instalike', 'myself', 'happy', 'f', 'followback', 'l', 'style', 'art',
+               'followers', 'likeforfollow', 'followforfollow', 'bhfyp', 'likeforlike', 'life', 'comment', 'cute',
+               'model', 'girl', 'nature', 'beauty', 'selfie', 'likesforlikes', 'fun', 'travel', 'photographer',
+               'motivation', 'music', 'makeup', 'memes', 'quotes', 'lifestyle', 'photoshoot', 'portrait', 'igers',
+               'following', 'insta', 'friends', 'fitness', 'tiktok', 'food', 'inspiration', 'share']
+
+tags_like = ['likes', 'like', 'follow', 'likeforlikes', 'love', 'instagood', 'instagram', 'followforfollowback',
+             'followme', 'photooftheday', 'bhfyp', 'instalike', 'photography', 'l', 'instadaily', 'me', 'picoftheday',
+             'beautiful', 'myself', 'likeforfollow', 'fashion', 'smile', 'followers', 'likeforlike', 'followback',
+             'f', 'followforfollow', 'comment', 'likesforlikes', 'bhfyp', 'happy', 'style', 'life', 'photo', 'nature',
+             'cute', 'insta', 'model', 'music', 'travel', 'likesforlike', 'girl', 'selfie', 'viral', 'loveyourself',
+             'following', 'memes', 'beauty', 'liker', 'lfl', 'yourself', 'lifestyle', 'likeback', 'sad', 'thoughts',
+             'writer', 'photographer', 'k', 'photoshoot', 'india']
+
+tags = list(set(tags_like + tags_cats + tags_follow))
+
+locations = ['New York', 'Los Angeles, California', 'San Francisco, California',
+             'Paris, France', 'Alabama', 'Barcelona, Spain', 'Madrid, Spain', 'Melbourne, Australia',
+             'Sydney, Australia', 'Bangkok, Thailand', 'Jakarta, Indonesia', 'Amsterdam, Netherlands',
+             'Chicago, Illinois', 'Santiago, Chile', 'San Diego, California', 'Houston, Texas',
+             'Mexico City, Mexico']
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
+from time import sleep
+import random
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -6,10 +7,39 @@ from selenium.common.exceptions import NoSuchElementException
 from data import *
 
 
+def tag_randomizer(tag_list):
+    text = ''
+    used_tags = []
+
+    # post can contain <= 30 tags
+    for tag in range(29):
+        random_tag = random.choice(tag_list)
+        text += f'#{random_tag} '
+        used_tags.append(random_tag)
+        tag_list.remove(random_tag)
+
+    tag_list += used_tags
+
+    return text
+
+
+def text_randomizer(tag_list):
+    text = f"""Tag cat lovers 🐱
+Follow @catzz_of_ig to be featured 😉
+Don't miss new posts! Turn on notifications! ✔️
+
+#cattzofig {tag_randomizer(tag_list)}"""
+
+    return text
+
+
 class CreatorStudioBot:
 
     def __init__(self):
+        # write path to your geckodriver.exe in executable path
         self.driver = webdriver.Firefox(executable_path=r'')
+
+        # set username and password in file data.py
         self.username = username
         self.password = password
 
@@ -33,37 +63,101 @@ class CreatorStudioBot:
         driver.get(link)
 
         driver.find_element_by_id('media_manager_chrome_bar_instagram_icon').click()
-        time.sleep(1)
+        sleep(1)
 
         driver.find_element_by_xpath(
             '/html/body/div/div[1]/div[2]/div/div[2]/div/div/div/div[2]/div'
         ).click()
-        time.sleep(3)
+        sleep(4)
+
+        # switch active browser window
+        driver.switch_to.window(driver.window_handles[1])
 
         # username input
-        driver.switch_to.window(driver.window_handles[1])
         driver.find_element_by_name('username').send_keys(username)
-        time.sleep(1)
+        sleep(1)
 
         # password input
         driver.find_element_by_name('password').send_keys(password)
-        time.sleep(2)
+        sleep(2)
 
         driver.find_element_by_xpath(
             '/html/body/div[1]/section/main/div/div/div/div/form/div/div[3]/button'
         ).click()
-        time.sleep(3)
+        sleep(5)
 
         if self.xpath_exists('/html/body/div[1]/section/main/div/div/div/section/div/div[2]'):
             driver.find_element_by_xpath(
                 '/html/body/div[1]/section/main/div/div/div/div/button'
             ).click()
-            time.sleep(3)
+            sleep(3)
 
         driver.switch_to.window(driver.window_handles[0])
-        time.sleep(3)
+        sleep(3)
+
+    def create_new_post(self):
+        print('Creating new post...')
+        driver = self.driver
+
+        driver.find_element_by_xpath(
+            '/html/body/div[1]/div[1]/div/div[2]/div/div/div[2]/div[1]/div/div[1]'
+        ).click()
+        sleep(1)
+
+        driver.find_element_by_xpath(
+            '/html/body/div[3]/div[1]/div[1]/div/div/div[1]/div[2]/div/div[1]/div/div/div/div/div[2]/div/strong'
+        ).click()
+        sleep(3)
+
+    def upload_content(self, path):
+        print('Uploading content:', path.split('\\')[-1])
+        driver = self.driver
+
+        driver.find_element_by_xpath(
+            '/html/body/div[4]/div/div/div/div[2]/div[1]/div/div[5]/div/div/div/span'
+        ).click()
+        sleep(1)
+
+        driver.find_element_by_css_selector("input[type=file]").send_keys(path)
+        sleep(2)
+
+    def add_text(self, text):
+        driver = self.driver
+
+        driver.find_element_by_xpath(
+            '/html/body/div[4]/div/div/div/div[2]/div[1]/div/div[2]/div[1]/div/div/div[2]/div'
+        ).send_keys(text)
+        sleep(2)
+
+    def add_random_text(self):
+        driver = self.driver
+
+        driver.find_element_by_xpath(
+            '/html/body/div[4]/div/div/div/div[2]/div[1]/div/div[2]/div[1]/div/div/div[2]/div'
+        ).send_keys(text_randomizer(tags))
+        sleep(2)
+
+    def add_location(self, location):
+        driver = self.driver
+
+        driver.find_element_by_xpath(
+            '/html/body/div[4]/div/div/div/div[2]/div[1]/div/div[3]/span/label/input'
+        ).send_keys(location)
+        sleep(2)
+
+    def add_random_location(self):
+        driver = self.driver
+
+        driver.find_element_by_xpath(
+            '/html/body/div[4]/div/div/div/div[2]/div[1]/div/div[3]/span/label/input'
+        ).send_keys(random.choice(locations))
+        sleep(2)
 
 
 bot = CreatorStudioBot()
 bot.login()
+bot.create_new_post()
+bot.upload_content(random.choice(cats_content))
+bot.add_random_text()
+bot.add_random_location()
 # bot.close_browser()
